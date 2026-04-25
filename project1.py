0dc33614baadce861866d1ad337920b4c2c46795
garv <agarwalgarv156@gmail.com>
2026-04-25 16:28:52 +0530

    initial commit

---
+++ project1.py
@@ -0,0 +1,25 @@
+balance = 5000
+
+print("1. Check Balance")
+print("2. Deposit")
+print("3. Withdraw")
+
+choice = int(input("Enter choice: "))
+
+if choice == 1:
+    print("Balance =", balance)
+
+elif choice == 2:
+    amount = int(input("Enter deposit amount: "))
+    balance += amount
+    print("New Balance =", balance)
+
+elif choice == 3:
+    amount = int(input("Enter withdraw amount: "))
+    if amount <= balance:
+        balance -= amount
+        print("Remaining Balance =", balance)
+    else:
+        print("Insufficient Balance")
+else:
+    print("Invalid Choice")
